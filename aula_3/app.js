730892feff420ac42ffccafa084a7278e5bfcdc9
phenriq2 <pedro.modesto15@gmail.com>
2024-03-20 13:16:52 -0300

    Aula 4: aula sobre numeros aleatorios e ternarios

--- aula_3/app.js
+++ aula_3/app.js
@@ -1,20 +1,22 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 42;
-let tentativas = 3;
+let numeroSecreto = parseInt((Math.random() * 100 + 1)/2);
+let tentativas = 5;
 let index = 0;
 
 console.log('Número secreto', numeroSecreto);
 while (index < tentativas) {
     let chute = prompt('Escolha um número entre 1 e 50');
-    if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto (${numeroSecreto})`);
+    if (chute == numeroSecreto)
         break;
-    }
     else if (chute > numeroSecreto) {
         alert(`O número secreto é menor que ${chute}`);
     }
     else {
         alert(`O número secreto é maior que ${chute}`);
-        index++;
     }
+    index++;
 }
+
+alert(`Isso ai! Você descobriu o número secreto (${numeroSecreto})`);
+
+
